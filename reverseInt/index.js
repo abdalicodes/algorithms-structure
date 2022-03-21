1ec71a287b27c45972b0a681b48972f6de950f6e
Abolfazl Abdali <abolfazlandali0@gmail.com>
2022-03-21 16:53:00 +0330

    reverse Int

---
+++ reverseInt/index.js
@@ -0,0 +1,35 @@
+// Given an integer, return an integer with the digits
+// reversed.
+// --- Examples
+//   reverseInt(13) === 31
+//   reverseInt(404) === 404
+//   reverseInt(100) === 1
+//   reverseInt(-13) === -31
+//   reverseInt(-100) === -1
+
+function reverseInt(n) {
+  let reversedN = parseInt(n.toString().split("").reverse().join(""));
+  if (n < 0) reversedN = reversedN * -1;
+  return reversedN;
+}
+
+mocha.setup("bdd");
+const { assert } = chai;
+
+describe("Integer Reversal", () => {
+  it("reverseInt() works on positive numbers", () => {
+    assert.equal(reverseInt(3), 3);
+    assert.equal(reverseInt(13), 31);
+    assert.equal(reverseInt(100), 1);
+    assert.equal(reverseInt(1408), 8041);
+  });
+
+  it("reverseInt() works on negative numbers numbers", () => {
+    assert.equal(reverseInt(-3), -3);
+    assert.equal(reverseInt(-13), -31);
+    assert.equal(reverseInt(-100), -1);
+    assert.equal(reverseInt(-1408), -8041);
+  });
+});
+
+mocha.run();
